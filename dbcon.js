612a9ba4d93405d1e9e9aa8421973d56234d3c5d
johnsonm325 <johnsonm325@gmail.com>
2018-08-16 09:17:59 -0400

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_redmonb',
-  password        : '2120',
-  database        : 'cs361_redmonb'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 
 module.exports.pool = pool;
